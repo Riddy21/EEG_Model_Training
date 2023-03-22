a2e18b70b5451674c0943ab41d98e51668c25bca
Ridvan Song <56486612+Riddy21@users.noreply.github.com>
2023-03-21 21:09:15 -0400

    removing unnecessary data

--- h5-to-tflite.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-model = tf.keras.models.load_model('./models/trial4/attempt12.h5')
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-tflite_model = converter.convert()
-open("converted_model.tflite", "wb").write(tflite_model)
